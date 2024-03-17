e95d956d99892199a1b1017a8a3f56f8e62b5b29
WonderChaos <874699078@qq.com>
2024-03-17 22:20:14 +0800

    add:pb

--- mywork/geecache/http.go
+++ mywork/geecache/http.go
@@ -5,6 +5,8 @@ package geecache
 import (
 	"fmt"
 	"geecache/consistenthash"
+	pb "geecache/geecachepb"
+	"github.com/golang/protobuf/proto"
 	"io"
 	"log"
 	"net/http"
@@ -20,7 +22,11 @@ type httpGetter struct {
 }
 
 // Get 通过http获取缓存值
-func (h *httpGetter) Get(group string, key string) ([]byte, error) {
+func (h *httpGetter) Get(in *pb.Request, out *pb.Response) error {
+	var (
+		group = in.GetGroup()
+		key   = in.GetKey()
+	)
 	u := fmt.Sprintf(
 		"%v%v/%v",
 		h.baseURL,
@@ -28,20 +34,23 @@ func (h *httpGetter) Get(group string, key string) ([]byte, error) {
 		url.QueryEscape(key))
 	res, err := http.Get(u)
 	if err != nil {
-		return nil, err
+		return err
 	}
 	defer func() {
 		_ = res.Body.Close()
 	}()
 	if res.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("server returned: %v", res.Status)
+		return fmt.Errorf("server returned: %v", res.Status)
 	}
 	//bytes, err := ioutil.ReadAll(res.Body)
 	bytes, err := io.ReadAll(res.Body)
 	if err != nil {
-		return nil, fmt.Errorf("reading response body: %v", err)
+		return fmt.Errorf("reading response body: %v", err)
 	}
-	return bytes, nil
+	if err = proto.Unmarshal(bytes, out); err != nil {
+		return fmt.Errorf("decoding response body: %v", err)
+	}
+	return nil
 }
 
 const (
@@ -95,8 +104,13 @@ func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	body, err := proto.Marshal(&pb.Response{Value: view.ByteSlice()})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 	w.Header().Set("Content-Type", "application/octet-stream")
-	_, err = w.Write(view.ByteSlice())
+	_, err = w.Write(body)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
--- mywork/geecache/peers.go
+++ mywork/geecache/peers.go
@@ -1,5 +1,7 @@
 package geecache
 
+import pb "geecache/geecachepb"
+
 // PeerPicker 节点选择接口
 type PeerPicker interface {
 	PickPeer(key string) (peer PeerGetter, ok bool) //根据传入的key选择相应节点
@@ -7,5 +9,6 @@ type PeerPicker interface {
 
 // PeerGetter 节点获取（缓存值）接口
 type PeerGetter interface {
-	Get(group string, key string) ([]byte, error) //从对应group查找缓存值
+	// 将http通信的中间载体换成protobuf
+	Get(in *pb.Request, out *pb.Response) error //从对应group查找缓存值
 }
